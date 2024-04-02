5cec74bfca8cd5b51a775744a5c800453cee6510
Amenti Kenea <55615576+amenti4k@users.noreply.github.com>
2024-04-01 22:14:09 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,137 +1,52 @@
-# $streamlit run main.py
-# pip install streamlit langchain langchain_openai beautifulsoup4 python-dotenv chromadb
-
-# God's gift to datascientists like me that love products too (product ds > research ds)
-import streamlit as st
 import pandas as pd
 import numpy as np
-# for separating out the chat that happens between AIMessage and HumanMessage (makes it easier to read))
-from langchain_core.messages import AIMessage, HumanMessage
 
-# below is where we get the chunks of the website scapped with beautifulsoup4
-from langchain_community.document_loaders import WebBaseLoader
+# Expander section
+with st.expander("About"):
+  st.write("""Trying to add a data table, chart, sidebar button with 
+          ballons, an image, text input & exploring tabs!""")
 
-# to chunk the text into pieces 
-from langchain.text_splitter import RecursiveCharacterTextSplitter
+# Sidebar section
+with st.sidebar:
+  st.subheader('This is a Sidebar')
+  st.write('Button with Balloons 🎈')
+  if st.button('Click me!✨'):
+    st.balloons()
+  else:
+    st.write(' ')
 
-# to get the embeddings of the chunks so get chroma from the community packages
-from langchain_community.vectorstores import Chroma
-from langchain_openai import OpenAIEmbeddings, ChatOpenAI
+# Dataframe and Chart display section
+st.subheader('Interactive Data Table')
+df = pd.DataFrame(
+    np.random.randn(50, 3),  # generates random numeric values!
+    columns=["a", "b", "c"])
+st.dataframe(df) 
 
-#get the context chat prompt template
-from langchain_core.prompts import ChatPromptTemplate, MessagesPlaceholder
+st.subheader('Bar Chart 📊')
+st.bar_chart(df)
 
-#
-from langchain.chains import create_history_aware_retriever, create_retrieval_chain
-from langchain.chains.combine_documents import create_stuff_documents_chain
+# Image upload and text input section
+st.subheader('An Image')
+st.image('https://www.scoopbyte.com/wp-content/uploads/2019/12/tom-and-jerry.jpg')
+
+st.subheader('Text Input')
+greet = st.text_input('Write your name, please!')
+st.write('👋 Hey!', greet)
 
 
-import os 
+# Tabs section
+st.subheader('Tabs')
+tab1, tab2 = st.tabs(["TAB 1", "TAB 2"])
 
-my_secret = os.environ['OPENAI_API_KEY'] 
+with tab1:
+  st.write('WOW!')
+  st.image("https://i.gifer.com/DJR3.gif", width=400)
 
-def get_response(user_input):
-    # this should be backend and done once and for all 
-    retriever_chain = get_context_retriver_chain(st.session_state.vector_store) #This part just runs once and then we can just use the retriever chain to get #the response
-    conversation_rag_chain = get_conversational_rag_chain(retriever_chain) ##then ask the question and get the response
-    #return get_conversational_rag_chain
-    response = conversation_rag_chain.invoke({
-        "chat_history":st.session_state.chat_history, 
-        "input": user_query
-    })
-    return response ['answer'] #leave out the other bullshit I don't need
-
-# Getting the website vectorized
-# def get_vectorstore_from_url(url):
-# get the entire vector store
-def get_vectorstore_from_url(url):
-    # get the text in docuemnt form to enable debugging 
-    loader = WebBaseLoader(url)
-    document = loader.load()
-
-    #use the langchain splitter using the chunks 
-    text_splitter = RecursiveCharacterTextSplitter()#chunk_size=1000, chunk_overlap=0)
-    document_chunks = text_splitter.split_documents(document)
-
-    #create vector store from the chunks (where are we storing and where are we getting the embeddings)
-    vector_store = Chroma.from_documents(document_chunks, OpenAIEmbeddings())
-    return vector_store
-
-def get_context_retriver_chain(vector_store):
-    # initalize a language model 
-    llm = ChatOpenAI()
-    retriever = vector_store.as_retriever()
-
-    prompt = ChatPromptTemplate.from_messages([
-        #add the chat history into the new prompt by having the messages placeholder variable 
-        MessagesPlaceholder(variable_name = "chat_history"), 
-        #when you populate the prompt, the input changes
-        ("user", "{input}"),
-        ("user","Given the above conversation, generate a search query to look up in order to get information relevant to the conversation") #this is another prompt that enables the combination of #the new imput + the history 
-    ])
-
-    #uses the LLM + the vector + the prompt to create a chain
-    retriever_chain = create_history_aware_retriever(llm, retriever, prompt)
-    return retriever_chain
-
-def get_conversational_rag_chain(retriever_chain):
-    llm = ChatOpenAI()
-    prompt = ChatPromptTemplate.from_messages([
-        ("system", "Answer the user's questions based on the below context:\n\n{context}"),
-        MessagesPlaceholder(variable_name="chat_history"),
-        ("user", "{input}"),
-    ])
-    # answer the question based on what we have 
-    stuff_documents_chain = create_stuff_documents_chain(llm,prompt)
-
-    # now link to the retriever chain 
-    return create_retrieval_chain(retriever_chain, stuff_documents_chain)
-
-# page config
-st.set_page_config(page_title="Amenti's Website Chatter", page_icon = "🧿") 
-st.title("Yo! Paste in live websites, and chat with them.") 
-website_url = st.text_input("Website URL")
-
-# we're going to initalize a list for the chat history
-
-# sidebar
-df = pd.DataFrame({
-    "col1": np.random.randn(1000) / 50 + 37.76,
-    "col2": np.random.randn(1000) / 50 + -122.4,
-    "col3": np.random.randn(1000) * 100,
-    "col4": np.random.rand(1000, 4).tolist(),
-})
-
-with st.sidebar: 
-    st.write("a simple front to navigate the web faster and do your own deep dives and keep asking more questions. new ways of 'surfing' the web.  X...Amenti") 
-
-# Don't open the chatbox unless someone enters the website url
-if website_url is None or website_url == "":
-    st.error("Please enter a website URL (make sure it's the https link)")
-
-else:
-    # session state
-    if "chat_history" not in st.session_state:
-        st.session_state.chat_history = [
-            AIMessage(content="Hello, I am a bot. How can I help you?"),
-        ]
-
-    # this part is to just make sure we're not running our money dry 
-    if "vector_store" not in st.session_state:
-        st.session_state.vector_store = get_vectorstore_from_url(website_url)
-
-    # user input and history handling
-    user_query = st.chat_input("Type your message here...")
-    if user_query is not None and user_query != "": 
-        response = get_response(user_query)
-        st.session_state.chat_history.append(HumanMessage(content=user_query))
-        st.session_state.chat_history.append(AIMessage(content=response))
-
-    # conversation
-    for message in st.session_state.chat_history:
-        if isinstance(message, AIMessage):
-            with st.chat_message("AI"):
-                st.write(message.content)
-        elif isinstance(message, HumanMessage):
-            with st.chat_message("Human"):
-                st.write(message.content)
+with tab2:
+  st.write('Do you like ice cream? 🍨')
+  agree = st.checkbox('Yes! I love it')
+  disagree = st.checkbox("Nah! 😅")
+  if agree:
+    st.write('Even I love it 🤤')
+  if disagree:
+    st.write('You are boring 😒')
